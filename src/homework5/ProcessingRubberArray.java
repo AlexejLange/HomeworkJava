1d1176dd0d0a2ac28a972cf58296e419c4e6a458
Alexej <drozdov76@gmail.com>
2022-06-24 22:50:16 +0300

    Homework #5. Homework preparation.

---
+++ src/homework5/ProcessingRubberArray.java
@@ -0,0 +1,28 @@
+package homework5;
+
+public class ProcessingRubberArray {
+    public static void main(String[] args) {
+        RubberArray array = new RubberArray();
+        array.add(5);
+        array.add(10);
+        array.add(15);
+        array.add(20);
+        System.out.println(array.size() + ": " + array);
+        System.out.println(array.get(2));
+        System.out.println(array.getMax());
+        System.out.println(array.getMin());
+        System.out.println(array.average());
+        array.remove(0);
+        System.out.println(array.size() + ": " + array);
+        array.remove(2);
+        System.out.println(array.size() + ": " + array);
+        array.remove(1);
+        System.out.println(array.size() + ": " + array);
+        array.remove(0);
+        System.out.println(array.size() + ": " + array);
+        System.out.println(array.get(2));
+        System.out.println(array.getMax());
+        System.out.println(array.getMin());
+        System.out.println(array.average());
+    }
+}
---
+++ src/homework5/RubberArray.java
@@ -0,0 +1,92 @@
+package homework5;
+
+import java.util.Arrays;
+
+public class RubberArray {
+    private int[] array;
+
+
+    public void add(int value) {
+        if (array == null) {
+            array = new int[1];
+            array[0] = value;
+        } else {
+            int[] newArray = new int[array.length + 1];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            newArray[array.length] = value;
+            array = newArray;
+        }
+    }
+
+    public Integer get(int index) {
+        if (array != null) {
+            if (index < array.length) {
+                return array[index];
+            } else {
+                return null;
+            }
+        } else{
+            return null;
+        }
+    }
+
+    public int size() {
+        return array == null ? 0 : array.length;
+    }
+
+    public void remove(int index) {
+        if (array != null) {
+            if (index < array.length) {
+                int[] newArray = new int[array.length - 1];
+                System.arraycopy(array, 0, newArray, 0, index);
+                System.arraycopy(array, index + 1, newArray, index, array.length - index - 1);
+                array = newArray;
+            }
+        }
+    }
+
+    public Integer getMax() {
+        if (array.length > 0) {
+            int maxValue = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > maxValue) {
+                    maxValue = array[i];
+                }
+            }
+            return maxValue;
+        } else {
+            return null;
+        }
+    }
+
+    public Integer getMin() {
+        if (array.length > 0) {
+            int minValue = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < minValue) {
+                    minValue = array[i];
+                }
+            }
+            return minValue;
+        } else {
+            return null;
+        }
+    }
+
+    public Double average() {
+        if (array.length > 0) {
+            double sum = array[0];
+            for (int i = 1; i < array.length; i++) {
+                sum += array[i];
+            }
+            return sum / array.length;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return array == null ? "[]" : Arrays.toString(array);
+    }
+}
