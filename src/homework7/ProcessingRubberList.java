99c64f1225e57cfbd75cf9ac4b83b8528a85aa82
Alexej <drozdov76@gmail.com>
2022-07-03 22:29:59 +0300

    Homework #7. Homework completed.

--- src/homework7/ProcessingRubberList.java
+++ src/homework7/ProcessingRubberList.java
@@ -1,5 +1,11 @@
 package homework7;
 
+/**
+ * Java Pro. Homework #7
+ * @author Alexej Lange
+ * @version 03 Jul 2022
+ */
+
 public class ProcessingRubberList {
     public static void main(String[] args) {
         RubberList rl = new RubberList();
@@ -7,8 +13,26 @@ public class ProcessingRubberList {
         rl.add(2);
         rl.add(5);
         System.out.println(rl);
-        System.out.println(rl.remove(2));
+        rl.remove(2);
         System.out.println(rl);
+        System.out.println(rl.remove(0));
+        System.out.println(rl);
+        System.out.println("------------------------------");
+        rl.push(3);
+        rl.push(54);
+        rl.push(12);
+        System.out.println(rl);
+        System.out.println(rl.peek());
+        rl.pop();
+        System.out.println(rl);
+        System.out.println("------------------------------");
+        rl.offer(26);
+        rl.offer(15);
+        System.out.println(rl);
+        rl.poll();
+        System.out.println(rl.poll());
+        System.out.println(rl);
+        System.out.println("------------------------------");
         for (Object item : rl) {
             System.out.println(item);
         }
