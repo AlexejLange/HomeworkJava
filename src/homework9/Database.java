0fad4b6b7a4e2ecbc36920ded0cc913ea54ad515
Alexej Langeemail <drozdov76@gmail.com>
2022-07-08 14:21:38 +0300

    Homework #9. Step 2. List and Map.

--- src/homework9/Database.java
+++ src/homework9/Database.java
@@ -1,18 +1,52 @@
 package homework9;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Database {
-    List<Employee> employeeList = new ArrayList<>();
+    protected List<Employee> employees = new ArrayList<>();
+    protected Set<String> positions =new HashSet<>();
 
-    public List<Employee> getEmployeeList() {
-        employeeList.addAll(Arrays.asList(
-                new Employee("John", "director", 1500, 46),
+    public void init() {
+        List<Employee> addEmployees =Arrays.asList(
+                new Employee("John", "director", 1250, 46),
                 new Employee("Joy", "assistant", 750, 23),
-                new Employee("Mark", "engineer", 1300, 42)
-        ));
-        return employeeList;
+                new Employee("Mark", "engineer", 1050, 42),
+                new Employee("Karl", "engineer", 1025,38)
+        );
+        employees.addAll(addEmployees);
+        for (Employee employee : addEmployees) {
+            positions.add(employee.getPosition());
+        }
+    }
+
+    public void create(Employee employee) {
+        employees.add(employee);
+        positions.add(employee.getPosition());
+    }
+
+    public String read() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < employees.size(); i++) {
+            sb.append(i).append(": ").append(employees.get(i)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public void update(int  index, Employee employee) {
+        employees.set(index, employee);
+        positions.add(employee.getPosition());
+    }
+
+    public void delete(int index) {
+        employees.remove(index);
+    }
+
+    public String getPositions() {
+        StringBuilder sb = new StringBuilder();
+        for (String position : positions) {
+            sb.append(position).append("\n");
+        }
+        return sb.toString();
     }
 }
+
--- src/homework9/UseEmployeesDatabase.java
+++ src/homework9/UseEmployeesDatabase.java
@@ -1,11 +1,47 @@
 package homework9;
 
+import java.util.Scanner;
+
 public class UseEmployeesDatabase {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Database database = new Database();
-        for (Employee employee : database.getEmployeeList()) {
-           System.out.println(employee);
-       }
+        database.init();
 
+        boolean exit = false;
+        String name, position;
+        int salary, age, index;
+
+        while (!exit) {
+            System.out.println("db> ");
+            String command = scanner.next();
+            switch (command) {
+                case "x" -> exit = true;
+                case "c" -> {
+                    System.out.println("create> ");
+                    name = scanner.next();
+                    position = scanner.next();
+                    salary = scanner.nextInt();
+                    age = scanner.nextInt();
+                    database.create(new Employee(name, position, salary, age));
+                }
+                case "r" -> System.out.println(database.read());
+                case "u" -> {
+                    System.out.println("update> ");
+                    index = scanner.nextInt();
+                    name = scanner.next();
+                    position = scanner.next();
+                    salary = scanner.nextInt();
+                    age = scanner.nextInt();
+                    database.update(index, new Employee(name, position, salary, age));
+                }
+                case "d" -> {
+                    System.out.println("delete> ");
+                    index = scanner.nextInt();
+                    database.delete(index);
+                }
+                case "p" -> System.out.println(database.getPositions());
+            }
+        }
     }
 }
