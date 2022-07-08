d6dd64f894dbb840e58e8d8debd849f72ab9f58c
Alexej Langeemail <drozdov76@gmail.com>
2022-07-08 19:35:22 +0300

    Homework #9. Step 3. Added Map.

--- src/homework9/Database.java
+++ src/homework9/Database.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Database {
     protected List<Employee> employees = new ArrayList<>();
     protected Set<String> positions =new HashSet<>();
+    protected Map<String, Employee> employeeMap = new HashMap<>();
 
     public void init() {
         List<Employee> addEmployees =Arrays.asList(
@@ -13,32 +14,50 @@ public class Database {
                 new Employee("Mark", "engineer", 1050, 42),
                 new Employee("Karl", "engineer", 1025,38)
         );
+
         employees.addAll(addEmployees);
+
         for (Employee employee : addEmployees) {
             positions.add(employee.getPosition());
+            employeeMap.put(employee.getName(),employee);
         }
     }
 
     public void create(Employee employee) {
         employees.add(employee);
         positions.add(employee.getPosition());
+        employeeMap.put(employee.getName(),employee);
     }
 
     public String read() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < employees.size(); i++) {
-            sb.append(i).append(": ").append(employees.get(i)).append("\n");
+        for (Employee employee : employeeMap.values()) {
+            sb.append(employee).append("\n");
         }
         return sb.toString();
     }
 
-    public void update(int  index, Employee employee) {
-        employees.set(index, employee);
-        positions.add(employee.getPosition());
+    public void update(String name, Employee employee) {
+        delete(name);
+        create(employee);
     }
 
-    public void delete(int index) {
-        employees.remove(index);
+    public void delete(String name) {
+        Employee employee = employeeMap.get(name);
+        if (employee != null) {
+            employeeMap.remove(employee.getName());
+            employees.remove(employee);
+        }
+    }
+
+    public String find(String name) {
+        Employee employee = employeeMap.get(name);
+        StringBuilder sb = new StringBuilder();
+        if (employee != null) {
+            sb.append(employee).append("\n");
+            return sb.toString();
+            }
+        return "Not found";
     }
 
     public String getPositions() {
--- src/homework9/UseEmployeesDatabase.java
+++ src/homework9/UseEmployeesDatabase.java
@@ -9,38 +9,49 @@ public class UseEmployeesDatabase {
         database.init();
 
         boolean exit = false;
-        String name, position;
-        int salary, age, index;
+        String name, position, oldName;
+        int salary, age;
 
         while (!exit) {
             System.out.println("db> ");
             String command = scanner.next();
             switch (command) {
-                case "x" -> exit = true;
-                case "c" -> {
+                case "x":
+                    exit = true;
+                    break;
+                case "c":
                     System.out.println("create> ");
                     name = scanner.next();
                     position = scanner.next();
                     salary = scanner.nextInt();
                     age = scanner.nextInt();
                     database.create(new Employee(name, position, salary, age));
-                }
-                case "r" -> System.out.println(database.read());
-                case "u" -> {
+                    break;
+                case "r":
+                    System.out.println(database.read());
+                    break;
+                case "u":
                     System.out.println("update> ");
-                    index = scanner.nextInt();
+                    oldName = scanner.next();
                     name = scanner.next();
                     position = scanner.next();
                     salary = scanner.nextInt();
                     age = scanner.nextInt();
-                    database.update(index, new Employee(name, position, salary, age));
-                }
-                case "d" -> {
+                    database.update(oldName, new Employee(name, position, salary, age));
+                    break;
+                case "d":
                     System.out.println("delete> ");
-                    index = scanner.nextInt();
-                    database.delete(index);
-                }
-                case "p" -> System.out.println(database.getPositions());
+                    name = scanner.next();
+                    database.delete(name);
+                    break;
+                case "f":
+                    System.out.println("find> ");
+                    name = scanner.next();
+                    System.out.println(database.find(name));
+                    break;
+                case "p":
+                    System.out.println(database.getPositions());
+                    break;
             }
         }
     }
